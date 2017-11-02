9f60ff61016488b95ae56d8bb351b210780963ab
Philipp Matveev <philipp.matveev@gmail.com>
2017-11-02 23:27:56 +0300

    #14 done

---
+++ Runcity/src/data.sql
@@ -0,0 +1,1 @@
+insert into consumer(id, username, credentials, email, is_active, passhash) values (1, 'admin', 'Administrator', 'admin@runcity.org', 1, '$2a$10$mVfrrJmajq8eud2fKg4UrupgG/FoPMin1Vk067IHqKaSxyvQWzpiG'); 
