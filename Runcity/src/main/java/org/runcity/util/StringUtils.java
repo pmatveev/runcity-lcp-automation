0d587770ba0b27d20da2ab926e95677a5442d80a
Philipp Matveev <philipp.matveev@gmail.com>
2017-12-05 21:10:27 +0300

    #17

--- Runcity/src/main/java/org/runcity/util/StringUtils.java
+++ Runcity/src/main/java/org/runcity/util/StringUtils.java
@@ -68,6 +68,6 @@ public class StringUtils {
 	
 	public static String xss(Object o) {
 		String s = o == null ? "" : o.toString();
-		return s.replace("<", "&lt;").replace(">", "&gt;");
+		return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;").replace("'", "&#x27").replace("/", "&#x2F");
 	}
 }
