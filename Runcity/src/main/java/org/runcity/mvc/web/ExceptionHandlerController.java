af5379efc831a2041bbd72628b375f7f50e656b9
Philipp Matveev <philipp.matveev@gmail.com>
2018-03-04 19:39:48 +0300

    #78 done

--- Runcity/src/main/java/org/runcity/mvc/web/ExceptionHandlerController.java
+++ Runcity/src/main/java/org/runcity/mvc/web/ExceptionHandlerController.java
@@ -10,26 +10,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExceptionHandlerController {
 	private static final Logger logger = Logger.getLogger(ExceptionHandlerController.class);
-	
+
+	@RequestMapping(value = "errors", method = RequestMethod.GET)
+	public String renderErrorPage(HttpServletRequest httpRequest) {
+		switch (getErrorCode(httpRequest)) {
+		case 404: {
+			return "exception/invalidUrl";
+		}
+		}
+		return "exception/exception";
+	}
+
+	private int getErrorCode(HttpServletRequest httpRequest) {
+		return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+	}
+
 	@ExceptionHandler(Throwable.class)
-	public ModelAndView handleException(HttpServletRequest request, HttpServletResponse response, HttpSession session, Throwable e) {
+	public ModelAndView handleException(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+			Throwable e) {
 		logger.fatal("Controller exception", e);
-		
+
 		if (StringUtils.isEqual(request.getContentType(), "application/x-www-form-urlencoded")) {
 			ModelAndView model = new ModelAndView("exception/exception");
 			model.addObject("errMsg", e.getMessage());
-	
+
 			return model;
 		}
-		
+
 		return null;
 	}
-	
+
 	@RequestMapping(value = "/403")
 	public String getAccessDenied(Model model) {
 		logger.info("GET 403");
